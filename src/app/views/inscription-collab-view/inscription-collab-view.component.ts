e3c0ff0466d283e7e92a2542b07e3aeccd80e8a5
uriel <uriel.vido@hotmail.fr>
2022-06-20 19:45:39 +0200

    form collab bug fix

--- src/app/views/inscription-collab-view/inscription-collab-view.component.ts
+++ src/app/views/inscription-collab-view/inscription-collab-view.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { NavigationService } from '../../services/navigation.service';
-
+import { MatRadioGroup } from '@angular/material/radio';
 @Component({
   selector: 'app-inscription-collab-view',
   templateUrl: './inscription-collab-view.component.html',
@@ -10,23 +10,32 @@ import { NavigationService } from '../../services/navigation.service';
 export class InscriptionCollabViewComponent implements OnInit {
   collabNewForm!: FormGroup;
   hide = true;
+  hide2 = true;
+
+
   constructor(private fb: FormBuilder,
     private navigationService: NavigationService) { }
-
   ngOnInit(): void {
     this.collabNewForm = this.fb.group({
       civilite: ['', Validators.required],
       nom: ['', Validators.required],
       prenom: ['', Validators.required],
       emailPro: ['', [Validators.required, Validators.email]],
-      fonction: ['',  Validators.required],
-      mdp: ['',  Validators.required],
-      mdp_confirm: ['',  Validators.required]
+      fonction: ['Docteur', Validators.required],
+      admin:[false],
+      mdp: ['',  [Validators.required, Validators.min(6)]],
+      mdp_confirm: ['', [Validators.required, Validators.min(6)]]
     });
+  }
+
+  validPswd():boolean{
+     return (this.collabNewForm.controls['mdp'].valid && this.collabNewForm.controls['mdp_confirm'].valid)
+      && (this.collabNewForm.controls['mdp'].value ===  this.collabNewForm.controls['mdp_confirm'].value);
   }
 
   send(){
     const user = this.collabNewForm.value;
-    console.log(JSON.stringify(user));
+    console.log(JSON.stringify(user)
+   + ' /Pswd match = '+ this.validPswd());
   }
 }
